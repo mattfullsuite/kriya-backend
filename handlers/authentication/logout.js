6a257432a24ecf06e351214345106675a29e0ce9
matt <matt@fullsuite.ph>
2024-01-11 16:19:02 +0800

    clearing cookie modifieid

--- handlers/authentication/logout.js
+++ handlers/authentication/logout.js
@@ -1,6 +1,7 @@
 module.exports = function Logout(req, res) {
   if (req.session.user) {
     res.clearCookie('userId');
+    res.clearCookie('HRISUserCookie');
     req.session.destroy();
     res.send({loggedIn: false})
   }
