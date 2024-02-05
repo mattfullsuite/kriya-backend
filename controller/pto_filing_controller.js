dbc5468d0f708578d00103511918d49022274017
Matt <smattwilfred01@gmail.com>
2024-02-05 13:43:39 +0800

    order by for approvers

--- controller/pto_filing_controller.js
+++ controller/pto_filing_controller.js
@@ -95,7 +95,7 @@ function ReturnTemporaryPTO(req, res) {
 
 function AllApprovers(req, res) {
     const uid = req.session.user[0].emp_id
-    const q = "SELECT * FROM emp JOIN dept ON emp_id = manager_id JOIN division ON dept.div_id = division.div_id WHERE emp.emp_role = 3 AND emp.emp_id != ?"
+    const q = "SELECT * FROM emp JOIN dept ON emp_id = manager_id JOIN division ON dept.div_id = division.div_id WHERE emp.emp_role = 3 AND emp.emp_id != ? ORDER BY emp.f_name"
 
     db.query(q,[uid],
         (err,data)=> {
