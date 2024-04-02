3c3faf47c4c13571fd921ed6e2f37040d000d071
matt <matt@fullsuite.ph>
2024-04-02 11:30:10 +0800

    fixed directory

--- controller/directory_controller.js
+++ controller/directory_controller.js
@@ -1,23 +1,10 @@
 var db = require("../config.js");
 
-// function GetDirectory(req, res) {
-//     const cid = req.session.user[0].company_id
-//     const q = "SELECT * FROM emp INNER JOIN emp_designation ON emp.emp_id = emp_designation.emp_id INNER JOIN position ON emp_designation.position_id = position.position_id INNER JOIN dept ON dept.dept_id = position.dept_id INNER JOIN division ON division.div_id=dept.div_id WHERE emp_designation.company_id = ?"
-
-//     db.query(q, cid, (err, data) => {
-//         if (err){
-//             console.log(err)
-//         } else {
-//             res.json(data)
-//         }
-//     })
-// }
-
 function GetDirectory(req, res) {
     const cid = req.session.user[0].company_id
-    const q = "SELECT emp_id, f_name, s_name, superior_id FROM emp WHERE date_separated IS NULL"
+    const q = "SELECT * FROM emp INNER JOIN emp_designation ON emp.emp_id = emp_designation.emp_id INNER JOIN position ON emp_designation.position_id = position.position_id INNER JOIN dept ON dept.dept_id = position.dept_id INNER JOIN division ON division.div_id=dept.div_id WHERE emp_designation.company_id = ?"
 
-    db.query(q, (err, data) => {
+    db.query(q, cid, (err, data) => {
         if (err){
             console.log(err)
         } else {
@@ -25,6 +12,19 @@ function GetDirectory(req, res) {
         }
     })
 }
+
+// function GetDirectory(req, res) {
+//     const cid = req.session.user[0].company_id
+//     const q = "SELECT emp_id, f_name, s_name, superior_id FROM emp WHERE date_separated IS NULL"
+
+//     db.query(q, (err, data) => {
+//         if (err){
+//             console.log(err)
+//         } else {
+//             res.json(data)
+//         }
+//     })
+// }
 
 function GetDivision(req, res) {
     const cid = req.session.user[0].company_id
