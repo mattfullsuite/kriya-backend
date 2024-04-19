c241905830a2e46290a9db17979635d28a37249d
matt <matt@fullsuite.ph>
2024-04-19 14:48:10 +0800

    ADD: Feature for attendance cross referencing
    Cross referencing features for ATtendance with Leaves

--- routes/attendance.js
+++ routes/attendance.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var imports =  {
     InsertAttendanceIntoDatabase,
-    GetLimitedAttendance
+    GetLimitedAttendance,
+    GetAttendance,
+    GetMyLeaves,
 } = require( "../controller/attendance_controller.js");
 
 const router = express.Router()
 
 router.post("/mtaa-insertAttendanceData", InsertAttendanceIntoDatabase);
 router.get("/mtaa-getLimitedAttendanceData", GetLimitedAttendance);
+router.get("/mtaa-getAttendanceData", GetAttendance);
+router.get("/mtaa-getLeaveData", GetMyLeaves)
 
 module.exports = router;
