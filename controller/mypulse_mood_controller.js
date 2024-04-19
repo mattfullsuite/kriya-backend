77376bfe427f4adaf781bb3ccfca9b4d12538db4
Matt <smattwilfred01@gmail.com>
2024-04-19 09:55:15 +0800

    ADD: Functions for MyPulse
    Finished My Pulse functions

--- controller/mypulse_mood_controller.js
+++ controller/mypulse_mood_controller.js
@@ -1,10 +1,28 @@
 var db = require("../config.js");
 var moment = require("moment");
 
+function InsertMoodEntry(req, res) {
+    var uid = req.session.user[0].emp_id;
+    var me = req.body.chosenMood;
+
+    console.log(JSON.stringify(req.body.chosenMood))
+    const values = [me, uid]
+
+    const q = "INSERT INTO pulse_mood (`mood_entry`, `emp_id`) VALUES (?)"
+
+    db.query(q, [values], (err, data) => {
+        if (err){
+            console.log(err)
+        } else {
+            console.log("Added.")
+        }
+    })
+}
+
 function GetMostRecentMood(req, res) {
     var uid = req.session.user[0].emp_id
 
-    const q = "SELECT * FROM pulse_mood WHERE emp_id = ? ORDER BY date_of_entry DESC LIMIT 1"
+    const q = "SELECT * FROM pulse_mood WHERE emp_id = ? ORDER BY date_of_entry DESC"
 
     db.query(q, [uid, moment()], (err, data) => {
         if (err){
@@ -73,10 +91,71 @@ function GetMoodsRecentMonth(req, res){
     }) 
 }
 
+function GetAverageMoodForWeek(req, res){
+    var uid = req.session.user[0].emp_id
+
+    const startOfWeek = moment().startOf('week').format('YYYY-MM-DD hh:mm');
+    const endOfWeek   = moment().endOf('week').format('YYYY-MM-DD hh:mm');
+
+    const q = "SELECT AVG(mood_entry) AS mood_average FROM pulse_mood WHERE emp_id = ? AND (date_of_entry > ?) AND (date_of_entry < ?)"
+
+    db.query(q, [uid, startOfWeek, endOfWeek], (err,data)=> {
+        if(err) console.log("err " + err)
+        return res.json(data)
+    }) 
+}
+
+function GetAverageMoodForLastWeek(req, res){
+    var uid = req.session.user[0].emp_id
+
+    const startOfWeek = moment().subtract(1, 'weeks').startOf('week').format('YYYY-MM-DD hh:mm');
+    const endOfWeek   = moment().subtract(1, 'weeks').endOf('week').format('YYYY-MM-DD hh:mm');
+
+    const q = "SELECT AVG(mood_entry) AS mood_average FROM pulse_mood WHERE emp_id = ? AND (date_of_entry > ?) AND (date_of_entry < ?)"
+
+    db.query(q, [uid, startOfWeek, endOfWeek], (err,data)=> {
+        if(err) console.log("err " + err)
+        return res.json(data)
+    }) 
+}
+
+function GetAverageMoodForMonth(req, res){
+    var uid = req.session.user[0].emp_id
+
+    const startOfMonth = moment().startOf('month').format('YYYY-MM-DD hh:mm');
+    const endOfMonth   = moment().endOf('month').format('YYYY-MM-DD hh:mm');
+
+    const q = "SELECT AVG(mood_entry) AS mood_average FROM pulse_mood WHERE emp_id = ? AND (date_of_entry > ?) AND (date_of_entry < ?)"
+
+    db.query(q, [uid, startOfMonth, endOfMonth], (err,data)=> {
+        if(err) console.log("err " + err)
+        return res.json(data)
+    }) 
+}
+
+function GetMostRecentMoodsLimited(req, res) {
+    var uid = req.session.user[0].emp_id
+
+    const q = "SELECT * FROM pulse_mood WHERE emp_id = ? ORDER BY date_of_entry DESC LIMIT 5"
+
+    db.query(q, [uid, moment()], (err, data) => {
+        if (err){
+            console.log(err)
+        } else {
+            res.json(data)
+        }
+    })
+}
+
 module.exports = {
     GetMostRecentMood,
     GetMoodsForDay,
     GetMoodsForWeek,
     GetMoodsLastWeek,
     GetMoodsRecentMonth,
+    InsertMoodEntry,
+    GetAverageMoodForWeek,
+    GetAverageMoodForMonth,
+    GetMostRecentMoodsLimited,
+    GetAverageMoodForLastWeek,
 };
--- routes/mypulse_mood.js
+++ routes/mypulse_mood.js
@@ -5,6 +5,11 @@ var imports =  {
     GetMoodsForWeek,
     GetMoodsLastWeek,
     GetMoodsRecentMonth,
+    InsertMoodEntry,
+    GetAverageMoodForWeek,
+    GetAverageMoodForMonth,
+    GetMostRecentMoodsLimited,
+    GetAverageMoodForLastWeek
 } = require( "../controller/mypulse_mood_controller.js");
 
 const router = express.Router()
@@ -13,5 +18,11 @@ router.get("/mp-getMostRecentMood", GetMostRecentMood);
 router.get("/mp-getMoodsForDay", GetMoodsForDay);
 router.get("/mp-getMoodForWeek", GetMoodsForWeek);
 router.get("/mp-getMoodLastWeek", GetMoodsLastWeek);
+router.post("/mp-addMood", InsertMoodEntry);
+router.get("/mp-getAverageWeekly", GetAverageMoodForWeek);
+router.get("/mp-getAverageLastWeek", GetAverageMoodForLastWeek);
+router.get("/mp-getAverageMonthly", GetAverageMoodForMonth);
+router.get("/mp-getMostRecentMoodsLimited", GetMostRecentMoodsLimited);
+
 
 module.exports = router;
