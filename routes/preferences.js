febac9805686441f3807d8c8886946379c63a95a
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-04-26 11:28:19 +0800

    Add: Functions
    Added functions for handling image upload. Added Function for inserting company.

--- routes/preferences.js
+++ routes/preferences.js
@@ -1,20 +1,27 @@
 var express = require("express");
-var imports =  {
-    CreateHoliday,
-    DeleteHoliday,
-    GetAllDivisions,
-    GetAllDepartments,
-    GetAllPositions,
-    GetManagersAndRespectiveDepartments,
-} = require( "../controller/preferences_controller.js");
+var uploadMulter = require("../handlers/utilities/multerFileHandler.js");
+var imports = ({
+  CreateHoliday,
+  CreateCompany,
+  DeleteHoliday,
+  GetAllDivisions,
+  GetAllDepartments,
+  GetAllPositions,
+  GetManagersAndRespectiveDepartments,
+} = require("../controller/preferences_controller.js"));
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/addHoliday', CreateHoliday);
+router.post(
+  "/addNewCompany",
+  uploadMulter.single("company_logo"),
+  CreateCompany
+);
+router.post("/addHoliday", CreateHoliday);
 router.delete("/holiday/:h_id", DeleteHoliday);
 router.get("/getAllDivisions", GetAllDivisions);
 router.get("/getAllDepartments", GetAllDepartments);
 router.get("/getAllPositions", GetAllPositions);
-router.get("/getManagersInEmpDesignation", GetManagersAndRespectiveDepartments)
+router.get("/getManagersInEmpDesignation", GetManagersAndRespectiveDepartments);
 
 module.exports = router;
