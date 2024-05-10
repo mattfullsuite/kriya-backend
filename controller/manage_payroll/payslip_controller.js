cb1387cdf9346f9bd4df3b9966a78a16f98c7183
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-05-10 11:06:48 +0800

    Remove: Obsolete Code

--- controller/manage_payroll/payslip_controller.js
+++ controller/manage_payroll/payslip_controller.js
@@ -36,40 +36,19 @@ const createPayslip = (req, res) => {
       generated_by,
     ];
   });
+
   const query = db.query(
     `INSERT INTO payslip (company_id, emp_num, last_name, first_name, middle_name, email, net_salary, dates, payables, totals, generated_by) VALUES ?;`,
     [dataProcessed],
-    async (error, response) => {
+    (error, data) => {
       if (error) {
         console.error(error);
-        response.sendStatus(500);
+        return res.sendStatus(500);
       } else {
-        response.sendStatus(200);
-        // const result = await generatePDF(data);
-        // console.log(result);
-        // if (result == 200) {
-        //   res.status(200).json(result);
-        // } else {
-        //   res.status(500).json({ "Error PDF: ": result });
-        // }
+        return res.sendStatus(200);
       }
     }
   );
-};
-
-const generatePDF = async (data) => {
-  console.log("Data to Generate: ", data);
-  console.log("Generating PDF!");
-
-  const result = await axios
-    .post(`https://pdf-generation-test.onrender.com/generate-and-send`, data)
-    .then(function (response) {
-      return response.status;
-    })
-    .catch(function (error) {
-      console.error("Error: ", error);
-    });
-  return result;
 };
 
 const getUserPayslip = (req, res) => {
@@ -111,7 +90,7 @@ const getAllPaySlip = (req, res) => {
   const uid = req.session.user[0].emp_num;
   const compID = req.session.user[0].company_id;
   const q =
-    "SELECT e.emp_num, CONCAT(e.f_name,' ', e.m_name,' ', e.s_name) AS 'emp_name',e.work_email,  e.date_hired, ps.dates, ps.payables, ps.totals, ps.net_salary, CONCAT(e2.f_name, ' ', e2.s_name) AS `generated_by`, ps.source, DATE_FORMAT(ps.`created_at`, '%m/%d/%Y %H:%i:%s') AS 'created_at' FROM `payslip` ps INNER JOIN emp e ON e.emp_num = ps.emp_num INNER JOIN `emp` e2 on e2.emp_num = ps.generated_by WHERE company_id = " +
+    "SELECT e.emp_num, CONCAT(e.f_name,' ', e.m_name,' ', e.s_name) AS 'emp_name',e.work_email, DATE_FORMAT( e.`date_hired`, '%m/%d/%Y') AS 'Date Hired', DATE_FORMAT(JSON_UNQUOTE(JSON_EXTRACT(`dates`, '$.From')),'%m/%d/%Y') AS `Date From`, DATE_FORMAT(JSON_UNQUOTE(JSON_EXTRACT(`dates`, '$.To')),'%m/%d/%Y') AS `Date To`, DATE_FORMAT(JSON_UNQUOTE(JSON_EXTRACT(`dates`, '$.Payment')),'%m/%d/%Y') AS `Date Payment`, ps.payables, ps.totals, ps.net_salary, CONCAT(e2.f_name, ' ', e2.s_name) AS `generated_by`, ps.source, DATE_FORMAT(ps.`created_at`, '%m/%d/%Y %H:%i:%s') AS 'created_at' FROM `payslip` ps INNER JOIN emp e ON e.emp_num = ps.emp_num INNER JOIN `emp` e2 on e2.emp_num = ps.generated_by WHERE company_id = " +
     compID +
     " ORDER BY `created_at` DESC;";
 
