b9a3d0eeb2a20f30a5c7c2cde6d018a72d87f004
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-05-24 11:11:54 +0800

    Add: Controller Function
    Created controller for getting user pay disputes.

--- routes/dispute.js
+++ routes/dispute.js
@@ -2,9 +2,13 @@ var express = require("express");
 const {
   createDispute,
   viewDisputes,
+  viewUserDisputes,
 } = require("../controller/dispute_controller");
 
 const router = express.Router();
 
 router.post("/d-createDispute", createDispute);
-router.get("/d-getAlldispute", viewDisputes);
+router.get("/d-getAllDispute", viewDisputes);
+router.get("/d-getUserdispute", viewUserDisputes);
+
+module.exports = router;
