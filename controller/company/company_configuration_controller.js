9456b981d2711b1d3f0f28da9ce3ac0775ad79cf
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-06-07 14:23:20 +0800

    Update: Company Configuration
    Updated company configuration controller and route.

--- controller/company/company_configuration_controller.js
+++ controller/company/company_configuration_controller.js
@@ -1,4 +1,3 @@
-const { Models } = require("openai/resources/models.mjs");
 var db = require("../../config.js");
 
 function GetCompanyConfiguration(req, res) {
@@ -13,10 +12,10 @@ function GetCompanyConfiguration(req, res) {
 function UpdateCompanyConfiguration(req, res) {
   const comp_id = req.session.user[0].company_id;
   const { configuration_value } = req.body;
-  const { configuration_id } = req.params;
+  const { id } = req.params;
   const q =
     "UPDATE `company_configuration` SET `configuration_value`= ? WHERE `company_configuration_id` = ? AND `company_id` = ?";
-  db.query(q, [configuration_value, configuration_id, comp_id], (err, data) => {
+  db.query(q, [configuration_value, id, comp_id], (err, data) => {
     if (err) return res.json(err);
     return res.sendStatus(200);
   });
