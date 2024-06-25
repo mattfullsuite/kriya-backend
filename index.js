fa7cfd9b852a9e63f3a5f98e8c4f399a0ceef4b2
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-06-25 09:05:21 +0800

    Update: CORS

--- index.js
+++ index.js
@@ -108,6 +108,7 @@ app.use(
   cors({
     origin: [
       process.env.ORIGIN_URL,
+      "https://www.tseksuite.com",
       "https://pdf-generation-test.onrender.com",
     ],
     methods: ["GET", "PATCH", "POST", "DELETE", "OPTIONS"],
