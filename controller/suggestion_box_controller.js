27f6338216e58d49aca4d52dd1c9b3d489a3922b
marvin <marvin@fullsuite.ph>
2024-07-03 09:17:02 +0800

    Modified sugggstion box controller

--- controller/suggestion_box_controller.js
+++ controller/suggestion_box_controller.js
@@ -2,10 +2,12 @@ var db = require("../config.js");
 
 function GetRequestMessages(req, res) {
   const uid = req.session.user[0].emp_id;
+  const requestID = req.params.request_id;
 
-  const q = "SELECT * FROM request WHERE emp_id = ?";
+  const q =
+    "SELECT * FROM suggestion_box_request WHERE request_id = ? AND emp_id = ?";
 
-  db.query(q, [uid], (err, data) => {
+  db.query(q, [requestID, uid], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
@@ -15,11 +17,19 @@ function GetRequestMessageContent(req, res) {
   const uid = req.session.user[0].emp_id;
   const requestID = req.params.request_id;
 
-  const q = "SELECT * FROM request WHERE request_id = ? AND emp_id = ?";
+  const q =
+    "SELECT * FROM suggestion_box_request WHERE request_id = ? AND emp_id = ?";
 
   db.query(q, [requestID, uid], (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
+    if (err) {
+      return res.json(err);
+    } else {
+      if(data.length != 0) {
+        return res.json(data);
+      } else {
+        return res.sendStatus(403);
+      }
+    }
   });
 }
 
@@ -28,14 +38,18 @@ function GetRequestMessageChat(req, res) {
   const requestID = req.params.request_id;
   const requesterID = req.params.emp_id;
 
-  const q =
-    "SELECT * FROM request_conversation WHERE request_id = ? AND requester_id = ?";
+  if (uid == requesterID) {
+    const q =
+      "SELECT * FROM suggestion_box_request_conversation WHERE request_id = ? AND requester_id = ?";
 
-  db.query(q, [requestID, requesterID], (err, data) => {
-    if (err) return res.json(err);
+    db.query(q, [requestID, requesterID], (err, data) => {
+      if (err) return res.json(err);
 
-    return res.json(data);
-  });
+      return res.json(data);
+    });
+  } else {
+    res.sendStatus(403);
+  }
 }
 
 module.exports = {
