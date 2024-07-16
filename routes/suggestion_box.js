55e57380db50f17be39684fa268388d900fd2caa
Marvin <marvin.directobautista@gmail.com>
2024-07-16 09:19:40 +0800

    Update: Currently working on suggestion box

--- routes/suggestion_box.js
+++ routes/suggestion_box.js
@@ -3,6 +3,9 @@ const {
   GetRequestMessages,
   GetRequestMessageChat,
   GetRequestMessageContent,
+  GetHr,
+  InsertRequest,
+  InsertRequestChat,
 } = require("../controller/suggestion_box_controller.js");
 
 const router = express.Router();
@@ -10,5 +13,9 @@ const router = express.Router();
 router.get("/sb-get-request", GetRequestMessages);
 router.get("/sb-get-request-conversation/:request_id/:emp_id", GetRequestMessageChat);
 router.get("/sb-get-request-content/:request_id", GetRequestMessageContent);
+router.get("/sb-get-hr", GetHr);
+
+router.post("/sb-insert-request", InsertRequest);
+router.post("/sb-insert-request-chat", InsertRequestChat);
 
 module.exports = router;
