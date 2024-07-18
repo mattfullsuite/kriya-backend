503214e8e114c910a2794c8a5e8e6e047832ed99
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-07-18 10:22:11 +0800

    Update: Controller
    Updated data handling. Updated Insert Query. Update Error handling for creating contribution.

--- controller/employee/employee_contributions_controller.js
+++ controller/employee/employee_contributions_controller.js
@@ -1,13 +1,10 @@
 var db = require("../../config.js");
 
 function CreateEmployeeContribution(req, res) {
-  console.log("Contributions Controller");
   const comp_id = req.session.user[0].company_id;
-  const data = req.body.contributions;
-  console.log("data: ", data);
-  console.log("Type of: ", typeof data);
-  console.log("Data: ", JSON.parse(JSON.stringify(data)));
-  const userID = req.body.emp_id;
+  const { employeeInfo } = req.body;
+  const data = employeeInfo.contributions;
+  const userID = employeeInfo.user_id;
 
   // Transform the data
   const formattedData = Object.entries(data).map(([key, value]) => {
@@ -16,9 +13,12 @@ function CreateEmployeeContribution(req, res) {
   });
 
   const q =
-    "INSERT INTO `employee_contributions`(`company_id, `emp_num`, `contribution_name`, `contribution_account_id`) VALUES ?";
-  db.query(q, [formattedData], (err, data) => {
-    if (err) return res.json(err);
+    "INSERT INTO emp_contributions (company_id, emp_id, contribution_name, contribution_account_id) VALUES ?";
+  db.query(q, [formattedData], (err, result) => {
+    if (err) {
+      console.log("Error: ", err);
+      return res.json(err);
+    }
     return res.send("success");
   });
 }
@@ -35,7 +35,7 @@ function UpdateEmployeeContribution(req, res) {
   });
 
   const q = `
-    INSERT INTO employee_contributions (company_id, emp_id, contribution_name, contribution_account_id)
+    INSERT INTO emp_contributions (company_id, emp_id, contribution_name, contribution_account_id)
     VALUES ?
     ON DUPLICATE KEY UPDATE contribution_account_id = VALUES(contribution_account_id);
   `;
@@ -53,7 +53,7 @@ function GetEmployeeContribution(req, res) {
   const emp_ID = req.params.id;
 
   const q =
-    "SELECT * FROM `employee_contributions` WHERE `company_id` = ? AND `emp_id` = ?";
+    "SELECT * FROM `emp_contributions` WHERE `company_id` = ? AND `emp_id` = ?";
   db.query(q, [comp_id, emp_ID], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
