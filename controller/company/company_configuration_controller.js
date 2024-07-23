cd6e188676a80b291a887e36cba2392f1012370d
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-07-23 13:44:47 +0800

    Update: Controller and Route
    Updated query and parameter.

--- controller/company/company_configuration_controller.js
+++ controller/company/company_configuration_controller.js
@@ -2,8 +2,10 @@ var db = require("../../config.js");
 
 function GetCompanyConfiguration(req, res) {
   const comp_id = req.session.user[0].company_id;
-  const q = "SELECT * FROM `company_configuration` WHERE `company_id` = ?";
-  db.query(q, comp_id, (err, data) => {
+  const { configuration_name } = req.params;
+  const q =
+    "SELECT * FROM `company_configuration` WHERE `company_id` = ? and `configuration_name` = ?";
+  db.query(q, [comp_id, configuration_name], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
