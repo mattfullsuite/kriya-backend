f530f226849743c134a754eadbb753846702dff2
marvin <marvin@fullsuite.ph>
2024-07-24 10:36:56 +0800

    Update: continue working on API endpoint of suggestion box

--- controller/suggestion_box_controller.js
+++ controller/suggestion_box_controller.js
@@ -2,11 +2,14 @@ const moment = require("moment");
 var db = require("../config.js");
 const { v4: uuidv4 } = require("uuid");
 
+// controllers for request feature in suggestion box
+
 function GetRequestMessages(req, res) {
   const uid = req.session.user[0].emp_id;
   const requestID = req.params.request_id;
 
-  const q = "SELECT * FROM suggestion_box_request WHERE requester_id = ? ORDER BY request_date DESC";
+  const q =
+    "SELECT * FROM suggestion_box_request WHERE requester_id = ? ORDER BY request_date DESC";
 
   db.query(q, [uid], (err, data) => {
     if (err) return res.json(err);
@@ -35,27 +38,12 @@ function GetRequestMessageContent(req, res) {
 }
 
 function GetRequestMessageChat(req, res) {
-  const uid = req.session.user[0].emp_id;
   const requestID = req.params.request_id;
-  const requesterID = req.params.emp_id;
 
   const q =
     "SELECT request_id, sender_id, request_chat, request_timestamp, f_name, s_name, emp_pic FROM suggestion_box_request_conversation AS sbc INNER JOIN emp ON sbc.sender_id = emp.emp_id  WHERE request_id = ?";
 
   db.query(q, [requestID], (err, data) => {
-    if (err) return res.json(err);
-
-    return res.json(data);
-  });
-}
-
-function GetHr(req, res) {
-  const uid = req.session.user[0].emp_id;
-
-  const q =
-    "SELECT emp_id, f_name, s_name FROM emp WHERE emp_role = 1 AND emp_id != ? AND date_separated IS NULL ORDER BY f_name ASC";
-
-  db.query(q, [uid], (err, data) => {
     if (err) return res.json(err);
 
     return res.json(data);
@@ -80,13 +68,7 @@ function InsertRequest(req, res) {
         console.log(err);
       }
 
-      q = "SELECT * FROM suggestion_box_request WHERE request_id = ?";
-
-      db.query(q, [request_id], (err, data) => {
-        if(err) console.log(err);
-
-        return res.json(data);
-      })
+      return res.sendStatus(200);
     }
   );
 }
@@ -96,12 +78,107 @@ function InsertRequestChat(req, res) {
   const sender_id = req.session.user[0].emp_id;
   const request_chat = req.body.request_chat;
 
-  const q = "INSERT INTO suggestion_box_request_conversation (request_id, sender_id, request_chat) VALUES (?, ?, ?)";
+  const q =
+    "INSERT INTO suggestion_box_request_conversation (request_id, sender_id, request_chat) VALUES (?, ?, ?)";
 
   db.query(q, [request_id, sender_id, request_chat], (err) => {
+    if (err) console.log(err);
+
+    return res.sendStatus(200);
+  });
+}
+
+// controllers for complaint feature in suggestion box
+
+function GetComplaintMessages(req, res) {
+  const uid = req.session.user[0].emp_id;
+  const complaintID = req.params.request_id;
+
+  const q =
+    "SELECT * FROM suggestion_box_complaint WHERE complainant_id = ? ORDER BY complaint_date DESC";
+
+  db.query(q, [uid], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+}
+
+function GetComplaintMessagesContent(req, res) {
+  const uid = req.session.user[0].emp_id;
+  const complaintID = req.params.complaint_id;
+
+  const q =
+    "SELECT * FROM suggestion_box_complaint WHERE complaint_id = ? AND complainant_id = ?";
+
+  db.query(q, [complaintID, uid], (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      if (data.length != 0) {
+        return res.json(data);
+      } else {
+        return res.sendStatus(404);
+      }
+    }
+  });
+}
+
+function GetComplaintMessageChat(req, res) {
+  const complaintID = req.params.complaint_id;
+
+  const q = "SELECT sbc.*, f_name, s_name, emp_pic FROM suggestion_box_complaint_conversation sbc INNER JOIN emp ON sbc.sender_id = emp.emp_id WHERE sbc.complaint_id = ?";
+
+  db.query(q, [complaintID], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.json(data);
+  });
+}
+
+function InsertComplaint(req, res) {
+  const complaint_id = uuidv4();
+  const complainant_id = req.session.user[0].emp_id;
+  const hr_id = req.body.hr_id;
+  const complaint_subject = req.body.complaint_subject;
+  const complaint_content = req.body.complaint_content;
+  const is_anonymous = req.body.is_anonymous;
+
+  const q = "INSERT INTO suggestion_box_complaint (complaint_id, complainant_id, hr_id, complaint_subject, complaint_content, is_anonymous) VALUES (?, ?, ?, ?, ?, ?)";
+
+  db.query(q, [complaint_id, complainant_id, hr_id, complaint_subject, complaint_content, is_anonymous], (err, data) => {
     if(err) console.log(err);
 
     return res.sendStatus(200);
+  })
+}
+
+function InsertComplaintChat(req, res) {
+  const complaint_id = req.body.complaintID;
+  const sender_id = req.session.user[0].emp_id;
+  const complaint_chat = req.body.complaint_chat;
+
+  const q = "INSERT INTO suggestion_box_complaint_conversation (complaint_id, sender_id, complaint_chat) VALUES (?, ?, ?)";
+
+  db.query(q, [complaint_id, sender_id, complaint_chat], (err, data) => {
+    if(err) console.log(err);
+
+
+    return res.sendStatus(200);
+  })
+}
+
+// controllers for utilities in suggestion box
+
+function GetHr(req, res) {
+  const uid = req.session.user[0].emp_id;
+
+  const q =
+    "SELECT emp_id, f_name, s_name FROM emp WHERE emp_role = 1 AND emp_id != ? AND date_separated IS NULL ORDER BY f_name ASC";
+
+  db.query(q, [uid], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.json(data);
   });
 }
 
@@ -109,7 +186,12 @@ module.exports = {
   GetRequestMessages,
   GetRequestMessageChat,
   GetRequestMessageContent,
-  GetHr,
   InsertRequest,
   InsertRequestChat,
+  GetComplaintMessages,
+  GetComplaintMessagesContent,
+  GetComplaintMessageChat,
+  InsertComplaint,
+  InsertComplaintChat,
+  GetHr,
 };
--- routes/suggestion_box.js
+++ routes/suggestion_box.js
@@ -6,16 +6,34 @@ const {
   GetHr,
   InsertRequest,
   InsertRequestChat,
+  GetComplaintMessages,
+  GetComplaintMessagesContent,
+  GetComplaintMessageChat,
+  InsertComplaintChat,
+  InsertComplaint,
 } = require("../controller/suggestion_box_controller.js");
 
 const router = express.Router();
-
+// get methods for requests
 router.get("/sb-get-request", GetRequestMessages);
-router.get("/sb-get-request-conversation/:request_id/:emp_id", GetRequestMessageChat);
+router.get("/sb-get-request-conversation/:request_id", GetRequestMessageChat);
 router.get("/sb-get-request-content/:request_id", GetRequestMessageContent);
-router.get("/sb-get-hr", GetHr);
 
+// post methods for requests
 router.post("/sb-insert-request", InsertRequest);
 router.post("/sb-insert-request-chat", InsertRequestChat);
+
+// get methods for complaints
+router.get("/sb-get-complaint", GetComplaintMessages);
+router.get("/sb-get-complaint-content/:complaint_id", GetComplaintMessagesContent);
+router.get("/sb-get-complaint-conversation/:complaint_id", GetComplaintMessageChat);
+
+// post methods for complaints
+router.post("/sb-insert-complaint", InsertComplaint);
+router.post("/sb-insert-complaint-chat", InsertComplaintChat);
+
+// get methods for utilities
+router.get("/sb-get-hr", GetHr);
+
 
 module.exports = router;
