02758e0f8ffe4c7a195493c055460cf6cecf1fbb
matt <matt@fullsuite.ph>
2024-07-24 15:38:14 +0800

    integrated applicant tracking system

--- routes/applicant_tracking.js
+++ routes/applicant_tracking.js
@@ -4,7 +4,13 @@ var imports =  {
     GetApplicantsFromDatabase,
     AddNewApplicant,
     ModifiedAddNewApplicant,
-    GetPositionsFromCompany
+    GetPositionsFromCompany,
+    GetPossibleReferrers,
+
+    //Edit
+    EditApplicantData,
+    GetNoteDetails,
+    InsertApplicantNotes
 } = require( "../controller/applicant_tracking_controller.js");
 
 const router = express.Router()
@@ -14,5 +20,11 @@ router.get("/ats-getApplicantsFromDatabase", GetApplicantsFromDatabase);
 router.post("/ats-addNewApplicant", AddNewApplicant);
 router.post("/ats-modifiedAddNewApplicant", ModifiedAddNewApplicant);
 router.get("/ats-getPositionsFromCompany", GetPositionsFromCompany);
+router.get("/ats-getPossibleReferrers", GetPossibleReferrers);
+router.post("/ats-editApplicantData", EditApplicantData);
+
+//Notes
+router.post("/ats-getNoteDetails", GetNoteDetails);
+router.post("/ats-insertApplicantNotes", InsertApplicantNotes);
 
 module.exports = router;
