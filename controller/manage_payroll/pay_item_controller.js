03707c1f6aedfb8d401da9021fc611fed84327d4
Deon Paul Sadcopen <deon@fullsuite.ph>
2024-08-21 13:41:43 +0800

    Update: Argument
    Included pay_item_1601c_tag in arguments.

--- controller/manage_payroll/pay_item_controller.js
+++ controller/manage_payroll/pay_item_controller.js
@@ -14,21 +14,21 @@ function getAllPayItems(req, res) {
 
 function addPayItem(req, res) {
   var cid = req.session.user[0].company_id;
-  const { name, category, type, group } = req.body;
+  const { name, category, type, group, tag_1601c } = req.body;
   const q =
-    "INSERT INTO pay_items(company_id, pay_item_name, pay_item_category, pay_item_type, pay_item_group) VALUES(?, ?, ?, ?, ?)";
-  db.query(q, [cid, name, category, type, group], (err, data) => {
+    "INSERT INTO pay_items(company_id, pay_item_name, pay_item_category, pay_item_type, pay_item_group, pay_item_1601c_tag) VALUES(?, ?, ?, ?, ?, ?)";
+  db.query(q, [cid, name, category, type, group, tag_1601c], (err, data) => {
     if (err) return res.json(err);
     return res.sendStatus(200);
   });
 }
 
 function updatePayItem(req, res) {
-  const { name, category, type, group } = req.body;
+  const { name, category, type, group, tag_1601c } = req.body;
   const { id } = req.params;
   const q =
-    "UPDATE pay_items SET pay_item_name = ?, pay_item_category = ?, pay_item_type = ?, pay_item_group = ? WHERE pay_items_id = ?";
-  db.query(q, [name, category, type, group, id], (err, data) => {
+    "UPDATE pay_items SET pay_item_name = ?, pay_item_category = ?, pay_item_type = ?, pay_item_group = ?, pay_item_1601c_tag = ? WHERE pay_items_id = ?";
+  db.query(q, [name, category, type, group, tag_1601c, id], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
   });
