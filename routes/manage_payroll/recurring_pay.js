9e1a41d4e2d7cdf8db575617bbfee272d8ef6bdf
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-10-24 10:42:02 +0800

    UPdate: Updated Imports

--- routes/manage_payroll/recurring_pay.js
+++ routes/manage_payroll/recurring_pay.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const {
   CreateRecurrringPay,
   GetAllRecurrringPay,
-  GetCertainRecurrringPay,
-  UpdateRecurrringPay,
+  GetEmployeeRecurringPay,
+  UpdateRecurringPay,
   GetActiveEmployeesRP,
   GetRecurringPayItems,
 } = require("../../controller/manage_payroll/recurring_pay_controller");
 
 router.get("/rp-GetAllRecurringPay", GetAllRecurrringPay);
-router.get("/rp-GetCertainRecurrringPay", GetCertainRecurrringPay);
+router.get("/rp-GetEmployeeRecurrringPay/:empID", GetEmployeeRecurringPay);
 router.get("/rp-GetActiveEmployeesRP", GetActiveEmployeesRP);
 router.post("/rp-CreateRecurrringPay", CreateRecurrringPay);
-router.patch("/rp-UpdateRecurrringPay", UpdateRecurrringPay);
+router.patch("/rp-UpdateRecurringPay", UpdateRecurringPay);
 router.get("/rp-GetRecurringPayItems", GetRecurringPayItems);
 
 module.exports = router;
