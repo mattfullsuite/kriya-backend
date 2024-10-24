5cc328aed11fedec9f0cd861d8e7bc734c134d4e
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-10-24 14:35:27 +0800

    Update: Order

--- controller/dispute_controller.js
+++ controller/dispute_controller.js
@@ -94,7 +94,7 @@ const viewDisputes = (req, res) => {
 const viewPayrollDisputes = (req, res) => {
   const cID = req.session.user[0].company_id;
   const q =
-    "SELECT e.emp_num, CONCAT(e.f_name, ' ', IF(e.m_name IS NOT NULL and e.m_name != '', LEFT(e.m_name, 1), 'N/A'), '.', ' ',e.s_name) AS 'name', p.position_name, d.dispute_id, d.dispute_type, d.requester_id, d.dispute_title, d.dispute_body, d.dispute_status, CONCAT(e2.f_name, ' ', IF(e2.m_name IS NULL OR e2.m_name = '', ' ', CONCAT(SUBSTRING(e2.m_name, 1, 1), '. ')), e2.s_name) AS 'handled_by' ,d.raised_at, d.closed_at FROM `dispute` d INNER JOIN emp e ON e.emp_id = d.requester_id INNER JOIN emp_designation ed ON ed.emp_id = e.emp_id INNER JOIN position p on p.position_id = ed.position_id LEFT JOIN emp e2 on e2.emp_id = d.handled_by  WHERE d.dispute_type LIKE 'Pay Dispute' AND ed.company_id = ?";
+    "SELECT e.emp_num, CONCAT(e.f_name, ' ', IF(e.m_name IS NOT NULL and e.m_name != '', LEFT(e.m_name, 1), 'N/A'), '.', ' ',e.s_name) AS 'name', p.position_name, d.dispute_id, d.dispute_type, d.requester_id, d.dispute_title, d.dispute_body, d.dispute_status, CONCAT(e2.f_name, ' ', IF(e2.m_name IS NULL OR e2.m_name = '', ' ', CONCAT(SUBSTRING(e2.m_name, 1, 1), '. ')), e2.s_name) AS 'handled_by' ,d.raised_at, d.closed_at FROM `dispute` d INNER JOIN emp e ON e.emp_id = d.requester_id INNER JOIN emp_designation ed ON ed.emp_id = e.emp_id INNER JOIN position p on p.position_id = ed.position_id LEFT JOIN emp e2 on e2.emp_id = d.handled_by  WHERE d.dispute_type LIKE 'Pay Dispute' AND ed.company_id = ? ORDER BY d.raised_at DESC";
   db.query(q, [cID], (err, rows) => {
     if (err) return res.json(err);
     return res.json(rows);
