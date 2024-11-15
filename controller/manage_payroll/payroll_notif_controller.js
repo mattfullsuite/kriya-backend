08754ccab1e714dd5af6e557308ea90c5c1d12db
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2024-11-15 13:56:21 +0800

    Create/Update: Controller & Route
    Created controller and route for deleting drafted payroll notif. Renamed controller and route from create_upload to payroll_notif.

--- controller/manage_payroll/payroll_notif_controller.js
+++ controller/manage_payroll/payroll_notif_controller.js
@@ -1,7 +1,7 @@
 var db = require("../../config.js");
 const { v7: uuidv7 } = require("uuid");
 
-// --------------------Payroll Notification--------------------
+// --------------------Start Of Payroll Notification--------------------
 // Get the required information of the employee
 exports.GetPayrollNotifRecordInfo = (req, res) => {
   const empID = req.params.empID;
@@ -34,22 +34,21 @@ exports.CreatePayrollNotifDraft = (req, res) => {
   db.query(query, values, (error, result) => {
     console.log("ERRor", error);
     console.log("Result", result);
-    if (error) return res.json(error);
-    return res.status(200).json(result);
+    if (error) return res.status(500).json(error);
+    return res.sendStatus(200);
   });
 };
 
 exports.CreatePayrollNotifDraftDate = (req, res, next) => {
   console.log(req.body);
   const dateUUID = uuidv7();
+  const compID = req.session.user[0].company_id;
   const { datePeriod } = req.body;
-  console.log("UUID:", dateUUID);
-  console.log("Date Period:", datePeriod);
   const query =
-    "INSERT INTO `payroll_notif_dates`(`payroll_notif_date_id`, `date_from`, `date_to`, `date_payment`) VALUES (?, ?, ?, ?)";
+    "INSERT INTO `payroll_notif_dates`(`payroll_notif_date_id`, `comp_id`, `date_from`, `date_to`, `date_payment`) VALUES (?, ?, ?, ?, ?)";
   db.query(
     query,
-    [dateUUID, datePeriod.From, datePeriod.To, datePeriod.Payment],
+    [dateUUID, compID, datePeriod.From, datePeriod.To, datePeriod.Payment],
     (error, result) => {
       console.log("ERRor", error);
       console.log("Result", result);
@@ -60,3 +59,27 @@ exports.CreatePayrollNotifDraftDate = (req, res, next) => {
     }
   );
 };
+
+// Check for Drafted Payroll Notif
+exports.CheckPayrollNotifRecords = (req, res, next) => {
+  const compID = req.session.user[0].company_id;
+  const { action } = req.params;
+
+  const query =
+    "SELECT e.`emp_num` AS 'Employee ID', e.work_email as `Email`, e.`s_name` AS 'Last Name', e.`f_name` AS 'First Name', e.`m_name` AS 'Middle Name', p.`position_name` AS 'Job Title', 	e.`date_hired` AS 'Hire Date',      pn.emp_num,      pn.pay_item_id,      pn.amount,      pnd.date_from,      pnd.date_to,      pnd.date_payment  FROM `payroll_notif_dates` pnd INNER JOIN `payroll_notif` pn ON pnd.payroll_notif_date_id = pn.date_id INNER JOIN emp e ON pn.emp_num = e.emp_num INNER JOIN emp_designation ed ON e.emp_id = ed.emp_id INNER JOIN `position` p ON p.position_id = ed.position_id  WHERE ed.company_id = ?";
+  db.query(query, [compID], (error, result) => {
+    if (error) return res.json(error);
+    return res.status(200).json(result);
+  });
+};
+
+exports.DeletePayrollNotifDraft = (req, res, next) => {
+  const compID = req.session.user[0].company_id;
+  const query = "DELETE FROM `payroll_notif_dates` WHERE `comp_id` = ?";
+  db.query(query, [compID], (error) => {
+    if (error) return res.json(error);
+    next();
+  });
+};
+
+// --------------------End Of Payroll Notification--------------------
---
+++ routes/manage_payroll/payroll_notif.js
@@ -0,0 +1,25 @@
+var express = require("express");
+const router = express.Router();
+
+var imports = ({
+  GetPayrollNotifRecordInfo,
+  CreatePayrollNotifDraft,
+  CreatePayrollNotifDraftDate,
+  CheckPayrollNotifRecords,
+  DeletePayrollNotifDraft,
+} = require("../../controller/manage_payroll/payroll_notif_controller"));
+
+router.get(
+  "/mp-pn-GetPayrollNotifRecordInfo/:empID",
+  GetPayrollNotifRecordInfo
+);
+router.post(
+  "/mp-pn-CreatePayrollNotifDraft",
+  DeletePayrollNotifDraft,
+  CreatePayrollNotifDraftDate,
+  CreatePayrollNotifDraft
+);
+router.get("/mp-pn-CheckPayrollNotifDraft/:action", CheckPayrollNotifRecords);
+router.delete("/mp-pn-DeletePayrollNotifDraft", DeletePayrollNotifDraft);
+
+module.exports = router;
