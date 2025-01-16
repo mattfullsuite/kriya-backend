9007d5675c17f973c92286fb7393f7444335e54d
Deon Paul Sadcopen <dpsadcopen@gmail.com>
2025-01-16 09:06:02 +0800

    Add: Controller and Route for Recurring Pay in Regular Payrun.

--- routes/manage_payroll/recurring_pay.js
+++ routes/manage_payroll/recurring_pay.js
@@ -7,6 +7,7 @@ const {
   UpdateRecurringPay,
   GetActiveEmployeesRP,
   GetRecurringPayItems,
+  RegularPayrollGetAllRecurrringPay,
 } = require("../../controller/manage_payroll/recurring_pay_controller");
 
 router.get("/rp-GetAllRecurringPay", GetAllRecurrringPay);
@@ -15,5 +16,9 @@ router.get("/rp-GetActiveEmployeesRP", GetActiveEmployeesRP);
 router.post("/rp-CreateRecurrringPay", CreateRecurrringPay);
 router.patch("/rp-UpdateRecurringPay", UpdateRecurringPay);
 router.get("/rp-GetRecurringPayItems", GetRecurringPayItems);
+router.get(
+  "/rp-RegularPayrollGetAllRecurrringPay",
+  RegularPayrollGetAllRecurrringPay
+);
 
 module.exports = router;
